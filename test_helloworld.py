a62fa7be3c7b43dbadd4fc8c903f8fd5e0dd3dbf
Ojelle Rogero <ojelle.rogero@gmail.com>
2021-07-09 15:36:00 +0800

    Initial Commit. Added sum function

---
+++ test_helloworld.py
@@ -0,0 +1,14 @@
+"""Test Script for Learning Git Commands
+   Created on July 8, 2021
+   Author: Ojelle Rogero
+   """
+
+def hello_world(str):
+    text = str
+    return text
+
+def add(x, y):
+    return x + y
+
+print(hello_world("Hello World!"))
+print("The sum of 10 and 15 is equal to ",  add(10, 15))
