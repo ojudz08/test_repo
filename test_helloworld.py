14cebbfc2a2ead8bace6927b2e7034268c45012a
Ojelle Rogero <ojelle.rogero@gmail.com>
2021-07-09 16:07:13 +0800

    With add func

--- test_helloworld.py
+++ test_helloworld.py
@@ -7,4 +7,8 @@ def test_print(str):
     text = str
     return text
 
+def add(x, y):
+    return x + y
+
 print(test_print("Hello World!"))
+print("The sum of 10 and 15 is equal to ", add(10, 15))
