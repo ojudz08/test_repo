9cb255430961ebdbb5fddb90e5f4ce38bf680688
Ojelle M. Rogero <ojelle.rogero@gmail.com>
2021-07-09 18:07:07 +0800

    Update test_helloworld.py

--- test_helloworld.py
+++ test_helloworld.py
@@ -3,10 +3,6 @@
    Author: Ojelle Rogero
    """
 
-def test_print(str):
-    text = str
-    return text
-
 def add(x, y):
     return x + y
 
@@ -19,7 +15,7 @@ def mult(x, y):
 def div(x, y):
     return x / y
 
-print(test_print("Hello World!"))
+   
 print("The sum of 10 + 15 is equal to ", add(10, 15))
 print("The difference of 10 - 15 is equal to ", sub(10, 15))
 print("The product of 10 x 15 is equal to ", mult(10, 15))
