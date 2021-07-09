2b23f2702f3345c29c3a87555f16d4b9a2503f8c
Ojelle M. Rogero <ojelle.rogero@gmail.com>
2021-07-09 17:11:01 +0800

    Update test_helloworld.py

--- test_helloworld.py
+++ test_helloworld.py
@@ -12,7 +12,11 @@ def add(x, y):
 
 def sub(x, y):
     return x - y
+   
+def mult(x, y):
+    return x * y
 
 print(test_print("Hello World!"))
 print("The sum of 10 and 15 is equal to ", add(10, 15))
-print("The diff of 10 and 15 is equal to ", sub(10, 15))
+print("The difference of 10 and 15 is equal to ", sub(10, 15))
+print("The product of 10 and 15 is equal to ", mult(10, 15))
